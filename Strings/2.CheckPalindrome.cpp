049ad47e8ae14b2c239ef850d674fec040227d62
Abdelsattar26 <m.abdelsattar6303@gmail.com>
2026-03-08 13:01:32 +0500

    Reverse and Palindrome

---
+++ Strings/2.CheckPalindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string palindrome = "LEVEL";
+    bool isPalindrome = true;
+
+    for (int i = 0; i < palindrome.length() / 2; i++)
+    {
+        if (palindrome[i] != palindrome[palindrome.length() - 1 - i])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+    {
+        cout << palindrome << " is Palindrome.";
+    }
+    else
+    {
+        cout << palindrome << " is not a Palindrome.";
+    }
+}
